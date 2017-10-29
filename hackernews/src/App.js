6e7e0e108a2953af3aa47a18d406f4ee4b22fd3b
Richard Shea <rshea@thecubagroup.com>
2017-10-29 21:25:00 +1300

    Make use of arrow functions in the context of the existing code

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -7,7 +7,7 @@ const list = [
     url: 'https://facebook.github.io/react/',
     author: 'Jordan Walke',
     num_comments: 3,
-    points: 4,
+    points: 9,
     objectID: 0,
   },
   {
@@ -15,7 +15,7 @@ const list = [
     url: 'https://github.com/reactjs/redux',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
-    points: 5,
+    points: 9,
     objectID: 1,
   },
 ];
@@ -24,8 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map(function(item) {
-          return(
+        {list.map(item => 
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -34,8 +33,7 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
             </div>
-          );
-        })}
+        )}
       </div>
     );
   }
