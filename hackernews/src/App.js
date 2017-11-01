2c74b1d5a78c88300821eaa976fe569b71d815b7
Richard Shea <rshea@thecubagroup.com>
2017-11-02 11:38:45 +1300

    Illustrate an alternative means of defining the click handler where the wrapping function for the desired action is no longer in the button definition but it's still within the map function block

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -59,30 +59,42 @@ class App extends Component {
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   render() {
-    return (
-      <div className="App">
-        <h2>{this.state.rightnow}</h2>
-        {this.state.list.map(item => 
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button"
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-        )}
+      return (
+          <div className="App">
+              {this.state.list.map(item => {
+                  /****************************************/
+                  const onHandleDismiss = () =>
+                      this.onDismiss(item.objectID);
+                  /****************************************/
+                  return (
+                      <div key={item.objectID}>
+                          <span>
+                              <a href={item.url}>{item.title}</a>
+                          </span>
+                          <span>{item.author}</span>
+                          <span>{item.num_comments}</span>
+                          <span>{item.points}</span>
+                          <span>
+                          <button
+                              onClick={onHandleDismiss}
+                              type="button"
+                          >
+                              Dismiss
+                          </button>
+                      </span>
+                  </div>
+              );
+          }
+          )}
       </div>
-    );
+      );
   }
+
 }
+
+
+
+
+
 
 export default App;
