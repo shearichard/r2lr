ddbd0d0678cc213cebb67fa3978de96d21247a0a
Richard Shea <rshea@thecubagroup.com>
2017-11-04 19:53:06 +1300

    Break 'Search' and 'Table' components out of 'App' component

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -7,7 +7,7 @@ const list = [
     url: 'https://facebook.github.io/react/',
     author: 'Jordan Walke',
     num_comments: 3,
-    points: 6,
+    points: 4,
     objectID: 0,
   },
   {
@@ -15,7 +15,7 @@ const list = [
     url: 'https://github.com/reactjs/redux',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
-    points: 6,
+    points: 4,
     objectID: 1,
   },
 ];
@@ -31,10 +31,56 @@ function isSearchedES5(searchTerm) {
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 //
+// S E A R C H  C O M P O N E N T
+//
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type='text'
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+//
+//  T A B L E  C O M P O N E N T
+//
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+//
+//  A P P  C O M P O N E N T
+//
 class App extends Component {
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   constructor(props) {
     super(props);
 
@@ -50,9 +96,6 @@ class App extends Component {
     this.onDismiss = this.onDismiss.bind(this);
 
   }
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   onSearchChange(event){
     this.setState({ searchTerm: event.target.value });
   } 
@@ -71,47 +114,21 @@ class App extends Component {
     const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
   } 
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   render() {
       const {searchTerm, list } = this.state;
       return (
-          <div className="App">
-              <form>
-                <input 
-                  type="text" 
-                  onChange={this.onSearchChange}
-                />
-              </form>
-              {list.filter(isSearched(searchTerm)).map(item => {
-                  //
-                  const onHandleDismiss = () =>
-                      this.onDismiss(item.objectID);
-                  //
-                  return (
-                      <div key={item.objectID}>
-                          <span>
-                              <a href={item.url}>{item.title}</a>
-                          </span>
-                          <span>{item.author}</span>
-                          <span>{item.num_comments}</span>
-                          <span>{item.points}</span>
-                          <span>
-                          <button
-                              onClick={onHandleDismiss}
-                              type="button"
-                          >
-                              Dismiss
-                          </button>
-                      </span>
-                  </div>
-              );
-          }
-          )}
-      </div>
+        <div className="App">
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+          <Table 
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        </div>
       );
   }
-
 }
 export default App;
