8f3147c4ed0c6d0fee2990293541e745e4d27378
Richard Shea <rshea@thecubagroup.com>
2017-11-07 12:16:20 +1300

    Implement Search, Button and Table as stateless functional components

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -7,7 +7,7 @@ const list = [
     url: 'https://facebook.github.io/react/',
     author: 'Jordan Walke',
     num_comments: 3,
-    points: 4,
+    points: 3,
     objectID: 0,
   },
   {
@@ -15,7 +15,7 @@ const list = [
     url: 'https://github.com/reactjs/redux',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
-    points: 4,
+    points: 2,
     objectID: 1,
   },
 ];
@@ -31,71 +31,49 @@ function isSearchedES5(searchTerm) {
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 //
-// B U T T O N  C O M P O N E N T
+// B U T T O N  F U N C T I O N A L  S T A T E L E S S  C O M P O N E N T
 //
-class Button extends Component {
-  render() {
-    const {
-      onClick,
-      className = '',
-      children,
-    } = this.props;
+const Button = ({ onClick, className, children }) => 
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
+//
+// S E A R C H  F U N C T I O N A L  S T A T E L E S S  C O M P O N E N T
+//
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type='text'
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    );
-  }
-}
 //
-// S E A R C H  C O M P O N E N T
+//  T A B L E  F U N C T I O N A L  S T A T E L E S S  C O M P O N E N T
 //
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children} <input
-          type='text'
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
-}
-//
-//  T A B L E  C O M P O N E N T
-//
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+const Table = ({ list, pattern, onDismiss }) =>
+    <div>
+      {list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <Button onClick={() => onDismiss(item.objectID)}>
+              Dismiss
+            </Button>
+          </span>
+        </div>
+      )}
+    </div>
 //
 //  A P P  C O M P O N E N T
 //
