254103c8bc642e5fa68a13fbb53a9170a5a9c4ed
Richard Shea <rshea@thecubagroup.com>
2017-11-10 23:23:28 +1300

    Switch search to initiate an API query rather than the filtration of the initial list as at current

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -22,10 +22,6 @@ const smallColumn = {
   width: '10%',
 };
 //
-//
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
-//
 // B U T T O N  F U N C T I O N A L  S T A T E L E S S  C O M P O N E N T
 //
 const Button = ({ onClick, className, children }) => 
@@ -39,23 +35,29 @@ const Button = ({ onClick, className, children }) =>
 //
 // S E A R C H  F U N C T I O N A L  S T A T E L E S S  C O M P O N E N T
 //
-const Search = ({ value, onChange, children }) =>
-  <form>
+const Search = ({ 
+  value, 
+  onChange, 
+  onSubmit, 
+  children 
+  }) =>
+  <form onSubmit={onSubmit}>
     {children} <input
       type='text'
       value={value}
       onChange={onChange}
+      
     />
   </form>
 
 //
 //  T A B L E  F U N C T I O N A L  S T A T E L E S S  C O M P O N E N T
 //
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
     <div className="table">
-      {list.filter(isSearched(pattern)).map(item =>
+      {list.map(item =>
         <div key={item.objectID} className="table-row">
-          <span style={midSmallColumn}>
+          <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
           </span>
           <span style={midColumn}>{item.author}</span>
@@ -87,6 +89,7 @@ class App extends Component {
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
 
   }
@@ -106,6 +109,11 @@ class App extends Component {
   }
   onSearchChange(event){
     this.setState({ searchTerm: event.target.value });
+  } 
+  onSearchSubmit(event){
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
   } 
   onDismiss(id){
     const isNotId = item => item.objectID !== id;
@@ -127,6 +135,7 @@ class App extends Component {
             <Search 
               value={searchTerm}
               onChange={this.onSearchChange}
+              onSubmit={this.onSearchSubmit}
             >
               Search
             </Search>
@@ -135,11 +144,8 @@ class App extends Component {
             &&
             <Table 
               list={result.hits}
-              pattern={searchTerm}
               onDismiss={this.onDismiss}
             />
-            :
-            null
           }
         </div>
       );
