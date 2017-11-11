7f65b7f69b6b869a65bbb82adfdb81302a1a97d6
Richard Shea <rshea@thecubagroup.com>
2017-11-11 16:57:12 +1300

    Introduce paginated fetch

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 //
 //
 const largeColumn = {
@@ -94,11 +97,25 @@ class App extends Component {
 
   }
   setSearchTopStories(result){
-    this.setState({ result });
+    const { hits, page } = result;
+
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      : [];
+      
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
+
   }
 
-  fetchSearchTopStories(searchTerm){
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0){
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(e => e);
@@ -125,30 +142,36 @@ class App extends Component {
     });
   } 
   render() {
-      const {searchTerm, result } = this.state;
+    const {searchTerm, result } = this.state;
+    const page = ( result && result.page ) || 0;
 
-      if (!result) { return null; }
+    if (!result) { return null; }
 
-      return (
-        <div className="page">
-          <div className="interactions">
-            <Search 
-              value={searchTerm}
-              onChange={this.onSearchChange}
-              onSubmit={this.onSearchSubmit}
-            >
-              Search
-            </Search>
-          </div>
-          { result
-            &&
-            <Table 
-              list={result.hits}
-              onDismiss={this.onDismiss}
-            />
-          }
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
         </div>
-      );
+        { result
+          &&
+          <Table 
+            list={result.hits}
+            onDismiss={this.onDismiss}
+          />
+        }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
+      </div>
+    );
   }
 }
 export default App;
