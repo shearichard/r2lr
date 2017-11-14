caaf06a23599e1938c1d3ad8c972b0ec287bb5e8
Richard Shea <rshea@thecubagroup.com>
2017-11-15 11:29:16 +1300

    Installed Enzyme and created unit tests using Enzyme

--- hackernews/src/App.test.js
+++ hackernews/src/App.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow }  from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App, { Search, Button, Table }  from './App';
+
+Enzyme.configure( { adapter: new Adapter() } );
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -30,8 +35,10 @@ describe('Search', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   })
+
 });
 describe('Button', () => {
+  
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button/>, div);
@@ -44,6 +51,30 @@ describe('Button', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   })
+
+  it('should contain a button element', () => {
+    const element = shallow(
+      <Button
+        className="fooClass"
+      >
+      Test Label
+      </Button>
+    );
+
+    expect(element.is('button')).toBe(true);
+  });
+  it('should contain the label passed to it', () => {
+    const element = shallow(
+      <Button
+        className="fooClass"
+      >
+      Test Label
+      </Button>
+    );
+
+    expect(element.text()).toBe("Test Label");
+  });
+
 });
 describe('Table', () => {
   const props = {
@@ -52,6 +83,7 @@ describe('Table', () => {
       { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
     ],
   };
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Table { ...props } />, div);
@@ -64,4 +96,12 @@ describe('Table', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   })
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
+  });
 });
